8b7d8151528351002c16be7b699698cb68776235
Richard Pomacosi <66580035+RichardPomacosi@users.noreply.github.com>
2020-07-16 00:32:56 -0400

    Add files via upload

---
+++ Pregunta_1/NET/WebApplication_ConsumidorPregunta1/WebForm1.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication_ConsumidorPregunta1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body style="height: 160px">
+    <form id="form1" runat="server">
+        <div>
+            CI</div>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" Text="Buscar DB" OnClick="Button1_Click" />
+        <asp:GridView ID="GridView1" runat="server" Height="167px" Width="224px">
+        </asp:GridView>
+        <p>
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Ver sin DB" />
+        </p>
+        <asp:TextBox ID="TextBox2" runat="server" Width="213px"></asp:TextBox>
+    </form>
+</body>
+</html>
---
+++ Pregunta_1/NET/WebApplication_ConsumidorPregunta1/WebForm1.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication_ConsumidorPregunta1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ServiceReference1.WebService1SoapClient ws = new ServiceReference1.WebService1SoapClient();
+            GridView1.DataSource = ws.Alumno();
+            GridView1.DataBind();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.WebService1SoapClient ws = new ServiceReference1.WebService1SoapClient();
+
+            string ci;
+            ci = TextBox1.Text;
+            GridView1.DataSource = ws.AlumnoNombre(ci);
+            GridView1.DataBind();
+           
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.WebService1SoapClient ws = new ServiceReference1.WebService1SoapClient();
+            TextBox2.Text = ws.nombreSinDB().ToString();
+
+        }
+    }
+}
